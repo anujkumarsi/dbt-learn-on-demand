0a2c8697e8810824f9921b89a93b24482b87a5e8
anuj.singh@consultant.underarmour.com <anuj.singh@consultant.underarmour.com>
2021-11-25 17:18:15 +0000

    Source configuration

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -5,7 +5,7 @@ with payments as (
     paymentmethod as payment_method,
     status,    
     Amount/100 as amount,
-    created as created_at from raw.stripe.payment 
+    created as created_at from {{source('stripe','payment')}} 
 )
 
 select * from payments
